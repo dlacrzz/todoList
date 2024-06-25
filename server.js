1d14fa5ce2608abf828af37fa8cbfdf87a563e6f
dlacrzz <122063410+dlacrzz@users.noreply.github.com>
2024-06-25 14:10:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'todo.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
