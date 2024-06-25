8a075fc3a8fecde42a02c755f68becd2fd42bc03
dlacrzz <122063410+dlacrzz@users.noreply.github.com>
2024-06-25 13:56:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'todo.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
